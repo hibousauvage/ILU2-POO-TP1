428b448873344b41d28fae810d4927d80460651e
hibousauvage <Lucas.lefebvre046@gmail.com>
2025-02-20 19:41:41 +0100

    TP1 classe interne marche

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -9,9 +9,9 @@ import villagegaulois.Village;
 public class Scenario {
 
 	public static void main(String[] args) {
-//		Village village = new Village("le village des irréductibles", 10, 5);
-//		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
-//		village.setChef(abraracourcix);
+		Village village = new Village("le village des irréductibles", 10, 5);
+		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
 		Druide druide = new Druide("Panoramix", 2, 5, 10);
 		Gaulois obelix = new Gaulois("Obélix", 25);
 		Gaulois asterix = new Gaulois("Astérix", 8);
@@ -26,15 +26,14 @@ public class Scenario {
 //		village.ajouterHabitant(abraracourcix);
 //		village.afficherVillageois();
 
-//		System.out.println(village.rechercherVendeursProduit("fleurs"));
-//		System.out.println(village.installerVendeur(bonemine, "fleurs", 20));
-//		System.out.println(village.rechercherVendeursProduit("fleurs"));
-//		System.out
-//				.println(village.installerVendeur(assurancetourix, "lyres", 5));
-//		System.out.println(village.installerVendeur(obelix, "menhirs", 2));
-//		System.out.println(village.installerVendeur(druide, "fleurs", 10));
+		System.out.println(village.rechercherVendeursProduit("fleurs"));
+		System.out.println(village.installerVendeur(bonemine, "fleurs", 20));
+		System.out.println(village.rechercherVendeursProduit("fleurs"));
+		System.out.println(village.installerVendeur(assurancetourix, "lyres", 5));
+		System.out.println(village.installerVendeur(obelix, "menhirs", 2));
+		System.out.println(village.installerVendeur(druide, "fleurs", 10));
 
-//		System.out.println(village.rechercherVendeursProduit("fleurs"));
+		System.out.println(village.rechercherVendeursProduit("fleurs"));
 //		Etal etalFleur = village.rechercherEtal(bonemine);
 //		System.out.println(etalFleur.acheterProduit(10, abraracourcix));
 //		System.out.println(etalFleur.acheterProduit(15, obelix));
--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -1,5 +1,7 @@
 package villagegaulois;
 
+import java.lang.reflect.Constructor;
+
 import personnages.Chef;
 import personnages.Gaulois;
 
@@ -8,10 +10,128 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche;
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	public Village(String nom, int nbVillageoisMaximum, int nbEtalMarche) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		this.marche = new Marche(nbEtalMarche);
+	}
+	
+	private static class Marche{
+		private Etal[] etals;
+		private int nbEtal;
+		
+		public Marche(int nbEtal) {
+			this.etals = new Etal[nbEtal];
+			this.nbEtal =nbEtal;
+			for(int i=0;i<this.nbEtal;i++) {
+				this.etals[i] = new Etal();
+			}
+		}		
+		
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			this.etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+		
+		private int trouverEtalLibre() {
+			for(int i=0;i<nbEtal;i++) {
+				if(!etals[i].isEtalOccupe()) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		private Etal[] trouverEtals(String produit) {
+			Etal[] etalsOccupe = new Etal[nbEtal];
+			Etal[] etalsOccupeR;
+			int nbEtalOccupe=0;
+			for(int i=0;i<nbEtal;i++) {
+				if(etals[i].contientProduit(produit)) {
+					etalsOccupe[nbEtalOccupe] = etals[i];
+					nbEtalOccupe++;
+				}
+			}
+			etalsOccupeR = new Etal[nbEtalOccupe];
+			for(int i=0;i<nbEtalOccupe;i++) {
+				etalsOccupeR[i] = etalsOccupe[i];
+			}
+			return etalsOccupeR;
+		}
+		private Etal trouverVendeur(Gaulois gaulois) {
+			for(int i=0;i<nbEtal;i++) {
+				if(etals[i].isEtalOccupe()) {
+					if(etals[i].getVendeur() == gaulois) {
+						return etals[i];
+					}
+				}
+			}
+			return null;
+		}
+		private String afficherMarche() {
+			StringBuilder s = new StringBuilder();
+			int nbEtalLibre=0;
+			for(int i=0;i<nbEtal;i++) {
+				if(etals[i].isEtalOccupe()) {
+					s.append(etals[i].afficherEtal());
+				}else {
+					nbEtalLibre++;
+				}
+			}
+			s.append("Il reste " + nbEtalLibre + "2tals non utilisés dans le marché.\n");
+			return s.toString();
+		}
+	}
+	
+	public String installerVendeur(Gaulois vendeur,String produit, int nbProduit) {
+		int nEtalLibre = marche.trouverEtalLibre();
+		StringBuilder s = new StringBuilder();
+		s.append(vendeur.getNom());
+		s.append(" cherche un endroit pour vendre ");
+		s.append(nbProduit);
+		s.append(" ");
+		s.append(produit);
+		s.append(".\n");
+		if(nEtalLibre != -1) {
+			marche.utiliserEtal(nEtalLibre, vendeur, produit, nbProduit);
+			s.append("Le vendeur ");
+			s.append(vendeur.getNom());
+			s.append(" vend des fleurs à l'étal n°");
+			s.append(nEtalLibre+1);
+			s.append(".\n");
+		}
+		return s.toString();
+	}
+	public String rechercherVendeursProduit(String produit) {
+		Etal[] etalVendProduit = marche.trouverEtals(produit);
+		int nbEtalVendProduit = etalVendProduit.length;
+		StringBuilder s = new StringBuilder();
+		switch (nbEtalVendProduit) {
+		case 0: {
+			
+			s.append("Il n'y a pas de vendeur qui propose des fleurs au marché.\n");
+			break;
+		}
+		case 1: {
+			s.append("Seul le vendeur ");
+			s.append(etalVendProduit[0].getVendeur().getNom());
+			s.append(" propose des ");
+			s.append(produit);
+			s.append(" au marché.\n");
+			break;
+		}
+		default:
+			s.append("Les vendeurs qui proposent des ");
+			s.append(produit);
+			s.append(" sont :\n");
+			for(int i=0;i<nbEtalVendProduit;i++) {
+				s.append("- ");
+				s.append(etalVendProduit[i].getVendeur().getNom());
+				s.append("\n");
+			}
+		}
+		return s.toString();
 	}
 
 	public String getNom() {
